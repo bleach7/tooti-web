fbcd997da5c81c197818c2c74b0b46a72e366ebc
Daulet Tulepbergenov <daulet6368@gmail.com>
2023-03-06 19:51:38 +0600

    style: add base, fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        lato: ["Lato", "system-ui"],
+        "san-francisco-pro-display": ["San Francisco Pro", "system-ui"],
+        "san-francisco-pro-text": ["San Francisco Text", "system-ui"],
+      },
+    },
   },
   plugins: [],
 };
